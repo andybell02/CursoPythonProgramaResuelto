a073cb8495e3076933e6db23d03b53b0b50cf9a6
andybell02 <98629326+andybell02@users.noreply.github.com>
2022-02-02 14:05:20 -0400

    Update 04 Variables como números - Ejercicio.py
    linea 25
    
    imprimir(podemos comprarlo)

--- 04 Variables como números - Ejercicio.py
+++ 04 Variables como números - Ejercicio.py
@@ -1,24 +1,25 @@
 #Estás con tus amigos y quieren comprar entre todos una botella de Tequila.
 #La botella de Tequila tiene un precio de 65 dólares.
 #En la línea 4, crea una variable llamada dineroTotal y asígnale el número 0.
-
+dineroTotal = 0
 #Tú tienes 14.50 dólares.
 #En la línea 7, crea una variable llamada miDinero con el dinero que tienes.
-
+miDinero = 14.58 
 #Tu amigo Luis tiene 17 dólares.
 #En la línea 10, crea una variable llamada dineroLuis con el dinero que él tiene.
-
+dineroLuis = 17
 #Tu amiga Valentina tiene 12.15 dólares.
 #En la línea 13, crea una variable llamada dineroValentina con el dinero que ella tiene.
-
+dineroValentina = 12.15
 #Tu amigo Daniel tiene 20 dólares.
 #En la línea 16, crea una variable llamada dineroDaniel con el dinero que él tiene.
-
+dineroDaniel = 23
 #Ahora, en la línea 20, vamos a contar el dinero que tienen entre todos.
 #Actualiza la variable dineroTotal para que sea igual a la suma del dinero de todos.
 #No uses los números directamente en la suma, sino las variables.
-
+dineroTotal += miDinero,dineroLuis,dineroValentina,dineroDaniel
 #En la línea 22, imprime la variable dineroTotal para saber cuánto dinero tienen.
-
+imprimir(dineroTtotal)
 #¿Tienen suficiente dinero para comprar la botella de Tequila?
 #En la línea 25 imprime un mensaje para decirle a tus amigos si pueden comprar la botella o no.
+imprimir
