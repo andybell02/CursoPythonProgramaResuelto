75e706c180b5b75f36d160a20cdb81844136dd53
andybell02 <98629326+andybell02@users.noreply.github.com>
2022-02-02 14:23:45 -0400

    Update 05 Actualizar Variables - Ejercicio.py

--- 05 Actualizar Variables - Ejercicio.py
+++ 05 Actualizar Variables - Ejercicio.py
@@ -1,25 +1,25 @@
 #Vas a ir de compras.
 #Inicialmente tienes 50 dólares disponibles para gastar.
 #En la línea 4, asigna el dinero que tienes a la variable dineroDisponible.
-
+dineroDisponible
 #Compraste una camiseta de 12.30 dólares.
 #En la línea 7, actualiza con el operador -= la variable dineroDisponible.
-
+dineroDisponible -= 12.30
 #Imprime en la línea 9 el dinero que te queda.
-
+imprimir = 50
 #Ahora compraste una lámpara para decorar tu casa. Te costó 30 dólares.
 #En la línea 12, usa el operador correspondiente para actualizar el dinero que te queda.
-
+-=30
 #En la línea 14, imprime el dinero que te queda.
-
+imprimir(30)
 #No te queda mucho dinero. Llamaste a un amigo que te prestó 20 dólares.
 #Añade, en la línea 17 y con el operador +=, esos 20 dólares a tu dinero disponible.
-
+dineroDisponible = +=20
 #¿Cuánto dinero tienes ahora? Imprímelo por pantalla en la línea 19.
-
+imprimir(dineroDisponible)
 #¡Te alcanza para unas gafas nuevas de 25 dólares! ¡Cómpralas!
 #En la línea 23, actualiza tu variable dineroDisponible para representar esta compra.
 #Luego, en la línea 24, imprime el dinero que te sobró.
-
-
+dineroDispomible -=25
+imprimir(dineroDisponible)
 #Si te quedaron 2.70 dólares, no solo hiciste bien el programa, ¡sino que puedes comprarte un postre!
